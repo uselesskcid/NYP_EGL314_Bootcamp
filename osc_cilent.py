1200bbd16c693046f346f3d83e5603fb78471c1f
uselesskcid <123967722+uselesskcid@users.noreply.github.com>
2024-04-23 22:24:04 +0800

    duma

--- osc_cilent.py
+++ osc_cilent.py
@@ -1,23 +1,78 @@
-# Huats 2023 oscstarterkit
 from pythonosc import udp_client
 
-def send_message(receiver_ip, receiver_port, address, message):
-	try:
-		# Create an OSC client to send messages
-		client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)
 
-		# Send an OSC message to the receiver
-		client.send_message(address, message)
 
-		print("Message sent successfully.")
-	except:
-		print("Message not sent")
+def QL1():
+
+    receive_ip = "192.168.254.184"
+
+    receive_port = 2084
+
+    addr = "/print"
+
+    message = "wassup"
+
+    send_message(receive_ip, receive_port, addr, message)
+
+
+
+def MA3():
+
+    receive_ip = "192.168.254.217"
+
+    receive_port = 5101
+
+    addr = "/print"
+
+    message = "wassup2" 
+
+    send_message(receive_ip, receive_port, addr, message)
+
+
+
+def send_message(receiver_ip, receiver_port, address, message):  # Corrected the function name
+
+    try:
+
+        # Create an OSC client to send messages
+
+        client = udp_client.SimpleUDPClient(receiver_ip, receiver_port)  # Corrected the variable name
+
+
+
+        # Send an OSC message to the receiver
+
+        client.send_message(address, message)  # Corrected the variable name
+
+
+
+        print("Message sent successfully.")
+
+    except:
+
+        print("Message not sent")
+
+
 
 # FOR INFO: IP address and port of the receiving Raspberry Pi
-PI_A_ADDR = "192.168.1.100"		# wlan ip
-PORT = 2000
+
+PI_A_ADDR = "192.168.254.184"  # wlan ip
+
+PORT = 2084
+
+
+
+PI_B_ADDR = "192.168.254.217"  # wlan ip
+
+PORT_B = 5101
+
+
 
 addr = "/print"
-msg = "salutations from pi_B"
 
-send_message(PI_A_ADDR, PORT, addr, msg)
+msg = "u nigga"
+
+
+
+send_message(PI_B_ADDR, PORT_B, addr, msg)  # Corrected the IP address and port
+
